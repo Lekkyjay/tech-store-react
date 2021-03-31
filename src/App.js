2094d0784c780860372a979a978a9cfa7b9418d3
Lekkyjay <lekkyjayjay@yahoo.com>
2021-03-31 22:48:23 +0200

    Route setup for pages.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Route, Switch } from 'react-router';
+import Home from './pages/Home';
+import About from './pages/About';
+import Cart from './pages/Cart';
+import Contact from './pages/Contact';
+import Products from './pages/Products';
+import SingleProduct from './pages/SingleProduct';
+import Default from './pages/Default';
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <div>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/cart' component={Cart} />
+        <Route path='/contact' component={Contact} />
+        <Route path='/products' exact component={Products} />
+        <Route path='/products/:id' component={SingleProduct} />
+        <Route component={Default} />
+      </Switch>
+    </div>
   );
 }
 
