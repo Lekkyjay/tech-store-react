7b4bab99119e9fc68a43ef4cdd39db31007787be
Lekkyjay <lekkyjayjay@yahoo.com>
2021-04-01 20:59:43 +0200

    Default page created using the Hero component.

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Hero from '../components/Hero'
+import defaultBcg from '../images/defaultBcg.jpeg'
 
 const Default = () => {
+  
   return (
-    <div>
-      <h1>Default Page</h1>
-    </div>
+    <Hero img={defaultBcg} title="404" max="true">
+      <h2 className="text-uppercase">page not found</h2>
+      <Link to="/" className="main-link" style={{ marginTop: "2rem" }}>
+        return home
+      </Link>
+    </Hero>
   )
 }
 
